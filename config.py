c74534bcd3586bb8615d478f5da5480d38da99cd
Madeline <mseif15@berkeley.edu>
2018-07-29 10:30:52 -0700

    first pass

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key = "RkyoIrJHccW1MeiTP9u3MYn1o"
+consumer_secret = "txufiwEw88HgdrOKf84VxUkHD8bYyhNvEP7xSB47SBzeZVVKRs"
+access_token = "1020113772596289536-DQikoYI0cz9ImQ3820AVVbHuPs14Ph"
+access_token_secret = "BrTXyyf0NgGltiVWw6H5CbsEzJkPvBlj7EBKUaHzNMR6J"
